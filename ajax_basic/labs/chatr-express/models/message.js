2d1b2b840374ea441a6e88508d09151aead64c7d
CalebFaulhaber <62074501+CalebFaulhaber@users.noreply.github.com>
2023-06-08 02:01:58 -0700

    Revert "Js ajax basics labs"

--- ajax_basic/labs/chatr-express/models/message.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = function(sequelize, DataTypes) {
-  const Message = sequelize.define("Message", {
-    body: DataTypes.TEXT,
-    flagged: DataTypes.BOOLEAN,
-    username: DataTypes.STRING
-  });
-  return Message;
-};
--- ajax_basic/labs/chatr-express/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {index} = require('../controllers/home');
-
-// 🌳 R O O T  P A T H
-router.get('/', index);
-
-module.exports = router;
