55c58c63690a01169c2d7d225dc99551eab5bae6
Taif Al Musabe <>
2023-07-04 12:10:01 -0700

    refactored question show page with hook

--- react_app/src/components/QuestionShowPage.js
+++ react_app/src/components/QuestionShowPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import { useEffect, useState } from "react"
 import QuestionDetails from "./QuestionDetails"
 import AnswerList from "./AnswerList"
 import { Question } from "../request"
@@ -11,55 +11,93 @@ export function withRouter(Childern) {
     })
 }
 
-class QuestionShowPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            questionData: {}
-        };
-    }
+const QuestionShowPage = (props) => {
+    const [questionData, setQuestionData] = useState({})
 
-    componentDidMount() {
-        Question.show(this.props.match.params.id)
+    useEffect(() => {
+        Question.show(props.match.params.id)
         .then((fetchedQuestion) => {
-            this.setState((state) => {
-               return  {questionData: fetchedQuestion}
-            })
+            setQuestionData(fetchedQuestion)
         })
-    }
-    deleteAnswerById = (answerId) => {
-        const filteredAnswers = this.state.questionData.answers.filter(x => x.id !== answerId);
-        let questionData = {
-            ...this.state.questionData,
-            answers: filteredAnswers
-        }
-        this.setState({
-            questionData: questionData
-        })
+    }, [])
+
+    const deleteAnswerById = (answerId) => {
+        const filteredAnswers = questionData.answers.filter(x => x.id !== answerId);
+        setQuestionData({...questionData, answers: filteredAnswers})
     }
 
-    render() {
-        const { id, title, body, author_name, view_count, created_at, updated_at, answers } = this.state.questionData;
-        return (
-            <main>
-                <QuestionDetails
-                    id={id}
-                    title={title}
-                    body={body}
-                    author_name={author_name}
-                    view_count={view_count}
-                    created_at={created_at}
-                    updated_at={updated_at}
-                    // If we want to send everything of the object (i.e. this.state), we will use this.state as deserializer
-                    // {...this.state}
-                />
-                <AnswerList
-                    answers={answers}
-                    deleteAnswer={this.deleteAnswerById}
-                />
-            </main>
-        )
-    }
+    const { id, title, body, author_name, view_count, created_at, updated_at, answers } = questionData;
+
+    return (
+        <main>
+            <QuestionDetails
+                id={id}
+                title={title}
+                body={body}
+                author_name={author_name}
+                view_count={view_count}
+                created_at={created_at}
+                updated_at={updated_at}
+                // If we want to send everything of the object (i.e. this.state), we will use this.state as deserializer
+                // {...this.state}
+            />
+            <AnswerList
+                answers={answers}
+                deleteAnswer={(id) => deleteAnswerById(id)}
+            />
+        </main>
+    )
 }
+
+// class QuestionShowPage extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             questionData: {}
+//         };
+//     }
+
+//     componentDidMount() {
+//         Question.show(this.props.match.params.id)
+//         .then((fetchedQuestion) => {
+//             this.setState((state) => {
+//                return  {questionData: fetchedQuestion}
+//             })
+//         })
+//     }
+//     deleteAnswerById = (answerId) => {
+//         const filteredAnswers = this.state.questionData.answers.filter(x => x.id !== answerId);
+//         let questionData = {
+//             ...this.state.questionData,
+//             answers: filteredAnswers
+//         }
+//         this.setState({
+//             questionData: questionData
+//         })
+//     }
+
+//     render() {
+//         const { id, title, body, author_name, view_count, created_at, updated_at, answers } = this.state.questionData;
+//         return (
+//             <main>
+//                 <QuestionDetails
+//                     id={id}
+//                     title={title}
+//                     body={body}
+//                     author_name={author_name}
+//                     view_count={view_count}
+//                     created_at={created_at}
+//                     updated_at={updated_at}
+//                     // If we want to send everything of the object (i.e. this.state), we will use this.state as deserializer
+//                     // {...this.state}
+//                 />
+//                 <AnswerList
+//                     answers={answers}
+//                     deleteAnswer={this.deleteAnswerById}
+//                 />
+//             </main>
+//         )
+//     }
+// }
 
 export default withRouter(QuestionShowPage)
