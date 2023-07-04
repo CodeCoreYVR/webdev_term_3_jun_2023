667a0fe84377dd585cfa14941cc1fb79ddf61726
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2023-07-03 18:57:09 -0700

    JS React Basics: Amazon React Review List

--- react-amazon-spa/src/components/ProductShowPage.jsx
+++ react-amazon-spa/src/components/ProductShowPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import productData from "../tempDB/productData";
 import ProductDetails from "./ProductDetails";
+import ReviewList from "./ReviewList";
 
 class ProductShowPage extends Component {
 	product = productData();
@@ -15,6 +16,14 @@ class ProductShowPage extends Component {
 				<h1 className="text-center">Product Show</h1>
 				<div className="card border-light mx-auto ">
 					<ProductDetails {...product} />
+					<div class="card-header bg-secondary text-white">
+						<h3 class="card-title">Reviews:</h3>
+					</div>
+					<ul class="list-group">
+						{product.reviews.length > 0 && (
+							<ReviewList reviews={product.reviews} />
+						)}
+					</ul>
 				</div>
 			</div>
 		);
