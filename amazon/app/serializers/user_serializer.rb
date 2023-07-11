3d1cca5418bf3aa1362a6fec0174220009c1d46b
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2023-07-10 18:43:58 -0700

    JS React Authentication: Amazon React Get Current User

--- amazon/app/serializers/user_serializer.rb
+++ amazon/app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   # Created with:
   # $ rails g serializer user 
 
-  attributes :id, :first_name, :last_name, :email, :full_name
+  attributes :id, :first_name, :last_name, :email, :full_name, :created_at, :updated_at
 end
