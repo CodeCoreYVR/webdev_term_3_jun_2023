e7b7ebca8695590f29115ec258254d85f55fd0b5
Taif Al Musabe <>
2023-08-01 10:41:09 -0700

    react-native pokemon payload fetch req with hook

--- react-native/pokedex/App.js
+++ react-native/pokedex/App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import { useEffect, useState } from 'react';
 
+const POKEMON_API_ENDPOINT =  'https://pokeapi.co/api/v2/pokemon?limit=100'
 export default function App() {
+
+  const [pokeList, setPokeList] = useState([])
+
+  useEffect(()=> {
+    fetch(`${POKEMON_API_ENDPOINT}`)
+      .then(res => res.json())
+      .then(payload => {
+        console.log(payload)
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Header/>
