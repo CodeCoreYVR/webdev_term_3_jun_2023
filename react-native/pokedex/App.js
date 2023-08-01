7e3ef1d75dd7be01cae3b1414bc2f968419ab931
Taif Al Musabe <>
2023-08-01 10:10:25 -0700

    react native styling of pokedex

--- react-native/pokedex/App.js
+++ react-native/pokedex/App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.h1}>Pokedex</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Hello World</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header : {
+    flex: 1,
+    backgroundColor: 'red',
+    width: '100%',
+    alignItems: 'center',
+    paddingTop: 25,
+    borderBottomWidth: 15
+  },
+  body : {
+    flex: 5
+  },
+  h1 : {
+    fontSize: 40
+  }
 });
